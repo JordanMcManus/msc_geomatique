7e36f71215233a7ca9eff6a45c877b328a5e2b9a
Jordan McManus <36419498+JordanMcManus@users.noreply.github.com>
2025-05-20 19:30:21 -0400

    added SBET fields

--- CALIBRATION_SPHERE/readSBET.m
+++ CALIBRATION_SPHERE/readSBET.m
@@ -10,3 +10,22 @@ function sbet = readSBET(filename)
 	
 	sbet = ( reshape( data,  nbDoublesInStruct, nbPointsInFile ) ).';
 end
+
+% SBET format
+% 1 	time;            //timetamp in seconds
+% 2 	latitude;        // radians
+% 3 	longitude;       // radians
+% 4 	ellipsoid_height;// meters
+% 5 	xSpeed;          //velocity in x direction
+% 6 	ySpeed;          //velocity in y direction
+% 7 	zSpeed;          //velocity in z direction
+% 8 	roll;            //roll angle
+% 9 	pitch;           //pitch angle
+% 10	heading;         //heading angle
+% 11	wander;          //wander
+% 12	xForce;          //force in x direction
+% 13	yForce;          //force in y direction
+% 14	zForce;          //force in z direction
+% 15	xAngularRate;	 //angular rate in x direction
+% 16	yAngularRate;    //angular rate in y direction
+% 17	zAngularRate;    //angular rate in z direction
