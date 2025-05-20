a5f54f6fb04a979e1243266264ec02ba225bd003
Jordan McManus <36419498+JordanMcManus@users.noreply.github.com>
2025-05-20 19:28:32 -0400

    fixed indent

--- CALIBRATION_SPHERE/readSBET.m
+++ CALIBRATION_SPHERE/readSBET.m
@@ -1,10 +1,10 @@
 function sbet = readSBET(filename)
-    nbDoublesInStruct = 17;
-    
-    fileID = fopen( filename );
+	nbDoublesInStruct = 17;
+	
+	fileID = fopen( filename );
 	data = fread( fileID, 'float64' );
 	fclose( fileID );
-
+	
 	nbDoublesInFile = length( data );
 	nbPointsInFile = nbDoublesInFile / nbDoublesInStruct;
 	
